24c0ab06a5db7cfa61984da1cbb8f356b4dbe7b7
galina <galina.ramodina@gmail.com>
2024-05-13 18:34:12 +0300

    + test

---
+++ src/test/java/hogwarts/school_files/SchoolFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package hogwarts.school_files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
